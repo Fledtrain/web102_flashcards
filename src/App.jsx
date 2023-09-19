caec8c1e6f2c0323ea7f954e1a0d56ebc146131f
Joey <Joeyew2014@gmail.com>
2023-09-19 13:24:07 -0400

    Installed TailwindCSS and created react app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1 className='text-4xl'>What Video Games character is this?</h1>
+        <h2>Lets test your video game knowledge!</h2>
+      </header>
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+
+const Questions = () => {
+    return (
+        <>
+            <section>
+                Question 1
+            </section>
+        </>
+    )
+}
+
+
+
+const Card = () => {
+
+    return (
+        <>
+            <disv>
+                <p>Card</p>
+                <Questions />
+            </disv>
+        </>
+    )
+}
+
+export default Card
+
