35ec1a538e3c9b87a07225a28a1f7c79695095e6
Fledtrain <Joeyew2014@gmail.com>
2023-09-26 17:53:20 -0400

    Added pictures for the questions and fixed random num generator

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const questions = [
     {
         id: 0,
-        question: "What character is this? He is the main protagonist of the Halo series.",
+        question: "What character is this? He is the main protagonist of the Halo series...",
         img: "MC.webp",
         answer: "Master Chief",
         difficulty: "easy"
@@ -12,21 +12,21 @@ const questions = [
     {
         id: 1,
         question: "How many Forza games are there?",
-        img: "",
+        img: "Forza.webp",
         answer: "5",
         difficulty: "medium"
     },
     {
         id: 2,
         question: "What is the most popular game in the world?",
-        img: "",
+        img: "Mine.webp",
         answer: "Minecraft",
         difficulty: "easy"
     },
     {
         id: 3,
         question: "What game is this character from? He is the main protagonist of the Legend of Zelda series.",
-        img: "",
+        img: "Link.webp",
         answer: "Link",
         difficulty: "easy"
     },
@@ -40,14 +40,14 @@ const questions = [
     {
         id: 5,
         question: "Which 1993 first-person shooter game is known for popularizing the genre? 🔫",
-        img: "",
+        img: "doom.webp",
         answer: "Doom",
         difficulty: "hard"
     },
     {
         id: 6,
         question: "What 2017 action-adventure game features a protagonist named Aloy?",
-        img: "",
+        img: "HZD.webp",
         answer: "Horizon Zero Dawn",
         difficulty: "hard"
     }
@@ -75,7 +75,27 @@ const Questions = ({ question, onNext, onPrev, onFlip, isFlipped }) => {
                     `}
                     onClick={onFlip}
                 >
-                    {isFlipped ? <><img src={question.img} alt="Test" /></> : question.question}
+                    {isFlipped ?
+                        <>
+                            <p>
+                                {question.answer}
+                            </p>
+                        </> :
+                        <>
+                            <div >
+                                <p className="p-5">
+                                    {question.question}
+                                </p>
+                                <img
+                                    // Center Image
+                                    className="rounded-2xl h-48 items-center justify-center m-auto"
+                                    src={question.img}
+                                    alt="No Hints"
+                                    loading="lazy"
+                                />
+                            </div>
+                        </>
+                    }
                 </div>
                 <div className="m-4">
                     {/* <button className="p-5 mr-2 hover:bg-slate-500 active:bg-slate-600 " onClick={onPrev}>Previous</button> */}
@@ -96,27 +116,26 @@ const Card = () => {
     const [questionNum, setQuestionNum] = useState(0)
     // Using this to check if the card is flipped or not
     const [isFlipped, setIsFlipped] = useState(false);
+    const [randomQuestionNum, setRandomQuestionNum] = useState(0)
 
     const flipCard = () => {
         setIsFlipped(!isFlipped)
     }
     const numOfCards = question.length
-    console.log("Number of cards", numOfCards)
-    // Creating a random number from 0 - 6
-    const randomNum = Math.floor(Math.random() * question.length)
 
     /** Increments Question Number
      * @returns {number}
-     */
+    */
     const nextCard = () => {
-        if (questionNum < numOfCards - 1) {
-            setQuestionNum(questionNum + 1)
-            setIsFlipped(false)
-        }
-        if (questionNum === numOfCards - 1) {
-            setQuestionNum(0)
-        }
-        console.log(randomNum)
+        let newRandomNum;
+        do {
+            newRandomNum = Math.floor(Math.random() * 7)
+        } while (newRandomNum === randomQuestionNum)
+
+        setRandomQuestionNum(newRandomNum)
+        setQuestionNum(newRandomNum)
+        setIsFlipped(false)
+
     }
     /**Decrements Question Number
      * @returns {number} 
