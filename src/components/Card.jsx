be9bdfb822eacad52cbea98c029b6dfc6c0faff6
Fledtrain <Joeyew2014@gmail.com>
2023-10-03 15:45:46 -0400

    Added optional feature for user answer being slightly different than actual answer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -69,22 +69,67 @@ const Questions = ({ question, onNext, onPrev, onFlip, isFlipped, onShuffle }) =
     const [streak, setStreak] = useState(0)
     const [longestStreak, setLongestStreak] = useState(0)
 
+    const calculateLevenshteinDistance = (a, b) => {
+        if (a.length === 0) return b.length;
+        if (b.length === 0) return a.length;
+        const matrix = [];
+        // Initialize matrix
+        for (let i = 0; i <= b.length; i++) {
+            matrix[i] = [i];
+        }
+        for (let j = 0; j <= a.length; j++) {
+            matrix[0][j] = j;
+        }
+        // Calculate Levenshtein distance
+        for (let i = 1; i <= b.length; i++) {
+            for (let j = 1; j <= a.length; j++) {
+                const cost = a[j - 1] === b[i - 1] ? 0 : 1;
+                matrix[i][j] = Math.min(
+                    matrix[i - 1][j] + 1, // Deletion
+                    matrix[i][j - 1] + 1, // Insertion
+                    matrix[i - 1][j - 1] + cost // Substitution
+                );
+            }
+        }
+        return matrix[b.length][a.length];
+    };
+
+    const isAnswerPartiallyCorrect = () => {
+        // Define a threshold for partial correctness
+        const partialCorrectThreshold = 2; // Adjust this threshold as needed
+
+        // Calculate Levenshtein distance between user's answer and the correct answer
+        const distance = calculateLevenshteinDistance(
+            answer.toLowerCase(), // Convert both answers to lowercase for case-insensitive comparison
+            question.answer.toLowerCase()
+        );
+        // Check if the Levenshtein distance is within the threshold
+        return distance <= partialCorrectThreshold;
+    };
+
     const handleChange = (e) => {
         setAnswer(e.target.value)
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        if (answer === question.answer) {
-            setColor(answer === question.answer && "border-purple-900 border-4")
-            setStreak(streak + 1)
-            setAnswer("")
+        e.preventDefault();
+        const isNumericAnswer = !isNaN(parseFloat(answer)) && isFinite(answer);
+        if (isNumericAnswer && parseFloat(answer) === parseFloat(question.answer)) {
+            // Numeric answer matches exactly
+            setColor(answer === question.answer && "border-purple-900 border-4");
+            setStreak(streak + 1);
+            setAnswer("");
+        } else if (!isNumericAnswer && (answer.toLowerCase() === question.answer.toLowerCase() || isAnswerPartiallyCorrect())) {
+            // Non-numeric answer is either an exact match or partially correct
+            setColor(answer === question.answer && "border-purple-900 border-4");
+            setStreak(streak + 1);
+            setAnswer("");
         } else {
-            setColor(answer !== question.answer && "border-4 border-red-900")
-            setLongestStreak(streak > longestStreak ? streak : longestStreak)
-            setStreak(0)
+            setColor("border-4 border-red-900");
+            setLongestStreak(streak > longestStreak ? streak : longestStreak);
+            setStreak(0);
         }
-    }
+    };
 
 
     return (
